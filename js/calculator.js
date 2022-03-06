ff97ed783c1f22f443653820d3a661c08b9c12f1
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-07 00:56:11 +0300

    create variables consistent with the variables in the day formula

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,10 @@
-function akanNameCalculator (number) {
-    let 
-}
+// declare variables used in the day formula
+var CC, YY, MM, DD, d, dayValue;
+var d = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+// validate user input
+
+
+
