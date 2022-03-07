8180eab3ac6b069fbf60f36e88505b431f453620
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-07 11:15:32 +0300

    add a function to validate whether a user has selected their gender

--- js/calculator.js
+++ js/calculator.js
@@ -4,4 +4,12 @@ var d = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-// validate user input
+// create a function that validates user gender
+function validateForm() {
+    let x = document.forms["myForm"]["gender"].value;
+    if (x == "") {
+    alert("Gender must be filled");
+          return false;
+        }
+      }
+      
