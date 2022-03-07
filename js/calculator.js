0d9d91765b7880fb660cc4f1c81848bd87f19319
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-07 22:50:10 +0300

    add CSS rule to style both the footer and the calculate button

--- js/calculator.js
+++ js/calculator.js
@@ -3,13 +3,3 @@ var CC, YY, MM, DD, d, dayValue;
 var d = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-
-// create a function that validates user gender
-function validateForm() {
-    let x = document.forms["myForm"]["gender"].value;
-    if (x == "") {
-    alert("Gender must be filled");
-          return false;
-        }
-      }
-      
