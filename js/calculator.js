04a6fa29c12229b76646b6b05f12913bb931478b
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-10 12:32:04 +0300

    take a screenshot of progress

--- js/calculator.js
+++ js/calculator.js
@@ -1,31 +1,28 @@
-// declare variables used in the day formula
-function calculate() {
-    let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-    alert (d)
+//prevent page from reloading when user submits info
+function formSubmit(){
+
+let date = document.getElementById("date").value
+let month = document.getElementById("month").value
+let year = document.getElementById("year").value
+let gender = document.getElementById("gender").value
+let CC = parseInt(year.slice(0,2));
+let YY = parseInt(year.slice(-2));
+let MM = parseInt(month);
+let DD = parseInt(date);
+let days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+let femaleNames = ["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"]
 }
 
 
-//create arrays to hold relevant data
-var day = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-var Gender = ["Female", "Male"]
+//validating user input
 
-// get date of birth and convert it into an integer
-function birthDate(){
-    var day=document.getElementById("DD").value;
-     var DD=parseInt(DD);
-}
+if (date < 1 || date >31) {
+    alert("Enter a valid date")
 
-// get birth month and convert it into an integer
-function birthMonth(){
-    var day=document.getElementById("MM").value;
-     var DD=parseInt(DD);
-}
 
-// get year and convert it into an integer
-function birthYear(){
-    var day=document.getElementById("YYYY").value;
-     var DD=parseInt(YYYY);
-}
+
+
+
+
 
