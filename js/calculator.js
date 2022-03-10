4befd9583daa441d1fb5a306cf5ee33322a7e06b
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-10 13:53:17 +0300

    create a function to limit the user input to a certain valid range

--- js/calculator.js
+++ js/calculator.js
@@ -15,10 +15,42 @@ let femaleNames = ["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"]
 }
 
 
-//validating user input
+//validating date input
 
-if (date < 1 || date >31) {
-    alert("Enter a valid date")
+// if (date < 1 || date > 31) {
+//     alert("Enter a valid date");
+// }
+// else {
+//     alert ("Date Accepted");
+// }
+
+// //validating month
+// if (month < 1 || month > 31) {
+//     alert("Enter a valid month");
+// }
+// else {
+//     alert ("Month Accepted");
+// }
+
+function validateMonth () {
+    if (month < 1 || month > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+function validateDay () {
+     if (date < 1 || dayOfBirth > 30){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+
+
 
 
 
