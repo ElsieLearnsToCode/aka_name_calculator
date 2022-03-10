c47277012a64e31747cc70502655380b813d472c
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-10 16:04:34 +0300

    add the final updates to the JS code

--- js/calculator.js
+++ js/calculator.js
@@ -1,59 +1,51 @@
 //prevent page from reloading when user submits info
 function formSubmit(){
+    let date = document.getElementById("date").value
+    let month = document.getElementById("month").value
+    let year = document.getElementById("year").value
+    let gender = document.getElementById("gender").value
+    let CC = parseInt(year.slice(0,2));
+    let YY = parseInt(year.substr(2,4));
+    let MM = parseInt(month);
+    let DD = parseInt(date);
+    let dayOfWeek = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+    let femaleNames = ["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"]
+    let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
-let date = document.getElementById("date").value
-let month = document.getElementById("month").value
-let year = document.getElementById("year").value
-let gender = document.getElementById("gender").value
-let CC = parseInt(year.slice(0,2));
-let YY = parseInt(year.slice(-2));
-let MM = parseInt(month);
-let DD = parseInt(date);
-let days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-let femaleNames = ["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"]
+// validating user input
+if (date < 1 || date > 31) {
+    alert("Enter a valid date");
+}
+ else {
+   alert ("Date Accepted");
+}
+
+//validating month
+if (month < 1 || month > 31) {
+    alert("Enter a valid month");
+}
+ else {
+ alert ("Month Accepted");
 }
 
 
-//validating date input
-
-// if (date < 1 || date > 31) {
-//     alert("Enter a valid date");
-// }
-// else {
-//     alert ("Date Accepted");
-// }
-
-// //validating month
-// if (month < 1 || month > 31) {
-//     alert("Enter a valid month");
-// }
-// else {
-//     alert ("Month Accepted");
-// }
-
-function validateMonth () {
-    if (month < 1 || month > 12) {
-      return false;
-    } else {
-      return true;
-    }
+if (d === 0) {
+  alert("Your name is" + "" + maleNames[0] + "" + "because you were born on a" + dayOfWeek[0])
+}
+else if (d ===0) {
+    alert("Your name is" + "" + femaleNames[0] + "" + "because you were born on a" + dayOfWeek[0])
 }
 
-function validateDate () {
-     if (date < 1 || date> 30){
-      return false;
-    } else {
-      return true;
-    }
+else {
+    alert ("enter valid details")
 }
 
-let validMonth = monthValidator();
-  let validDay = dayValidator();
 
-  // formula for calculating day
+}
 
-let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
+
 
 
 
