ad4ddab8f6b22c3927f95ce819f619c9dcd0b932
ElsieLearnsToCode <elsieakoth21@gmail.com>
2022-03-10 04:44:21 +0300

    capture a screenshot of the project to save the changes made this far

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,13 @@
 // declare variables used in the day formula
-let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+function calculate() {
+    let d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+    alert (d)
+}
+
 
 //create arrays to hold relevant data
 var day = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+
